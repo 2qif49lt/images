884d66e68deda1805e68f6ae9a9159d9d5f6a1ed
2qif49lt <xhb928@live.com>
2024-06-27 10:43:55 +0800

    main to master
    log
    
    mutli images support

--- pull.sh
+++ pull.sh
@@ -6,13 +6,18 @@ if [ -z $PREFIX ]; then
     exit 1
 fi
 
+replace_slash() {
+    input="$1"
+    echo $1 | sed 's/\//-s-/g'
+}
+
 file=trigger.txt
 
 # 循环读取文件的每一行并加上前缀字符串
 for line in `cat $file`
 do
-    new_line="$PREFIX/$line"
-    echo $new_line
+    new_line="$PREFIX/$(replace_slash $line)"
+    echo $new_line | sed -e 's/\(.\)/\1 /g'
 
     docker pull $line
     docker tag $line $new_line
